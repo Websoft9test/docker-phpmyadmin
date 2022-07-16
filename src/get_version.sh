f5fc03c28bf5ec29d75a0ebe780ba238ad8ab6d7
qiaofeng1227 <76487013@qq.com>
2022-07-16 11:39:00 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo Phpmyadmin Version:$(docker exec -i phpmyadmin bash -c 'cat /var/www/html/package.json | grep version') 1>> /data/logs/install_version.txt
+sudo echo Phpmyadmin Version:$(docker exec -i $1 bash -c 'cat /var/www/html/package.json | grep version') 1>> /data/logs/install_version.txt
